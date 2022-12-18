3f146cdfba1a25c1d6713c4db47d40343979505a
Arvydas Cereska <arvydas.cereska@stud.vilniustech.lt>
2022-12-18 22:47:16 +0200

    Refaktoringas

--- Arkanoid12/Arkanoid12.cpp
+++ Arkanoid12/Arkanoid12.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <time.h>
 #include <math.h>
 #include <string>
 #include <stdlib.h>
 #include <conio.h>
 #include <cstdlib>
+#include "Arkanoid12.h"
 
 using namespace std;
 
@@ -71,49 +71,11 @@ int main()
 	case KEY_PLAY:
 		while (true)
 		{
-			srand(time(NULL));
 			ballx = ballx + balldx;
 			bally = bally - changeBallY;
 
 			cout << "Score: " << score << "           Press X to quit :)" << endl;
-			for (int x = 0; x < 28; x++)
-			{
-				for (int y = 0; y < 66; y++)
-				{
-					if ((padx == x and y == pady) or (padx == x and y == pady - 1) or (padx == x and y == pady - 2) or (padx == x and y == pady - 3)
-						or (padx == x and y == pady + 1) or (padx == x and y == pady + 2) or (padx == x and y == pady + 3))
-					{
-						cout << padStr;
-					}
-					else if (ballx == x and bally == y and matrix[x][y] != 2) {
-						cout << ballStr;
-					}
-					else if (ballx == x and bally == y and matrix[x][y] == 2) {
-						cout << " ";
-						ballx = ballx + balldx;
-						matrix[x][y] = 3;
-						score += 1;
-					}
-					else if (ballx == x and bally == y and matrix[x][y] == 1) {
-						ballx = ballx + balldx;
-						bally = bally + changeBallY;
-					}
-					else if (matrix[x][y] == 1) {
-						cout << wallStr;
-					}
-					else if (matrix[x][y] == 2) {
-						cout << brickStr;
-					}
-					else if (matrix[x][y] == 3)
-					{
-						cout << " ";
-					}
-					else if (matrix[x][y] == 4) {
-						cout << score;
-					}
-				}
-				cout << endl;
-			}
+			Matrixdraw(padx, pady, padStr, ballx, bally, matrix, ballStr, balldx, score, changeBallY, wallStr, brickStr);
 
 			switch (_getch())
 			{
@@ -128,31 +90,14 @@ int main()
 				}
 				break;
 			case KEY_X:
-				system("CLS");
-				cout << "Your score was: " << score << endl;
-				cout << endl;
-				cout << endl;
-				cout << "Better luck next time";
-				return 0;
+				return EndGameBefore(score);
 			}
 			if (score == 275) {
-				system("CLS");
-				cout << "You have won the game good job buddy" << endl;
-				cout << "End score: " << score << endl;
-				cout << endl;
-				cout << endl;
-				cout << endl;
-				return 0;
+				return FinishGame(score);
 			}
 			if (ballx > 24)
 			{
-				system("CLS");
-				cout << "You have lost  the game, better luck next time" << endl;
-				cout << "End score: " << score << endl;
-				cout << endl;
-				cout << endl;
-				cout << endl;
-				return 0;
+				return LostGame(score);
 			}
 			if (matrix[ballx][bally-1] == 1 or matrix[ballx][bally + 2] == 1) {
 				changeBallY = changeBallY * -1;
@@ -171,13 +116,92 @@ int main()
 		}
 		break;
 	case KEY_END:
-		system("CLS");
-		cout << "You just left the game";
-		cout << endl;
-		cout << endl;
-		cout << endl;
-		return 0;
+		return LeftGame();
 	default:
 		break;
+	}
+}
+
+int LeftGame()
+{
+	system("CLS");
+	cout << "You have left the game" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	return 0;
+}
+
+int LostGame(int score)
+{
+	system("CLS");
+	cout << "You have lost  the game, better luck next time" << endl;
+	cout << "End score: " << score << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	return 0;
+}
+
+int FinishGame(int score)
+{
+	system("CLS");
+	cout << "You have won the game good job buddy" << endl;
+	cout << "End score: " << score << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	return 0;
+}
+
+int EndGameBefore(int score)
+{
+	system("CLS");
+	cout << "Your score was: " << score << endl;
+	cout << endl;
+	cout << endl;
+	cout << "Better luck next time";
+	return 0;
+}
+
+void Matrixdraw(int padx, int pady, string& padStr, int& ballx, int& bally, int  matrix[100][100], string& ballStr, int balldx, int& score, int changeBallY, string& wallStr, string& brickStr)
+{
+	for (int x = 0; x < 28; x++)
+	{
+		for (int y = 0; y < 66; y++)
+		{
+			if ((padx == x and y == pady) or (padx == x and y == pady - 1) or (padx == x and y == pady - 2) or (padx == x and y == pady - 3)
+				or (padx == x and y == pady + 1) or (padx == x and y == pady + 2) or (padx == x and y == pady + 3))
+			{
+				cout << padStr;
+			}
+			else if (ballx == x and bally == y and matrix[x][y] != 2) {
+				cout << ballStr;
+			}
+			else if (ballx == x and bally == y and matrix[x][y] == 2) {
+				cout << " ";
+				ballx = ballx + balldx;
+				matrix[x][y] = 3;
+				score += 1;
+			}
+			else if (ballx == x and bally == y and matrix[x][y] == 1) {
+				ballx = ballx + balldx;
+				bally = bally + changeBallY;
+			}
+			else if (matrix[x][y] == 1) {
+				cout << wallStr;
+			}
+			else if (matrix[x][y] == 2) {
+				cout << brickStr;
+			}
+			else if (matrix[x][y] == 3)
+			{
+				cout << " ";
+			}
+			else if (matrix[x][y] == 4) {
+				cout << score;
+			}
+		}
+		cout << endl;
 	}
 }
